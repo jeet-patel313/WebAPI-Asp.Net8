e8734eefb113fd16ac4bf23c2db6dd0b8c4772d8
jeetpatel313 <jp2398d@gmail.com>
2024-04-15 16:32:40 -0700

    Add project files.

---
+++ WebAPIDemo/Controllers/ShirtsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPIDemo.Models;
+
+namespace WebAPIDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ShirtsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetShirts()
+        {
+            return "Reading all the shirts";
+        }
+
+        [HttpGet("{id}")]
+        public string GetShirtById(int id)
+        {
+            return $"Reading shirt: {id}";
+        }
+
+        [HttpPost]
+        public string CreateShirt([FromBody]Shirt shirt)
+        {
+            return $"Creating a shirt";
+        }
+
+        [HttpPut("{id}")]
+        public string UpdateShirt(int id)
+        {
+            return $"Updating shirt: {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string DeleteShirt(int id)
+        {
+            return $"Deleting shirt: {id}";
+        }
+    }
+}
